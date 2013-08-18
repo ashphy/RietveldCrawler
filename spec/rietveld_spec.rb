09173be50613e83ce6081791e55743e1a5b57845
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2013-08-18 22:31:12 +0900

    Add spec for search request has 2pages

--- spec/rietveld_spec.rb
+++ spec/rietveld_spec.rb
@@ -10,17 +10,26 @@ describe Rietveld do
   end
 
   describe '#search' do
+
+    def get_site_url_with_cursor(cursor)
+      "#{conf[:base_url]}search?cursor=#{cursor}&format=json&keys_only=True&limit=1000"
+    end
+
     let(:site_url) {
       "#{conf[:base_url]}search?cursor&format=json&keys_only=True&limit=1000"
     }
 
-    let (:cursor) {
+    let(:normal_cursor) {
       'E-ABAOsB8gEIbW9kaWZpZWT6AQkI2J-H0PPvuALsAYICKWoXc35jaHJvbWl1bWNvZGVyZXZpZXctaHJyDgsSBUlzc3VlGI2S5goMFA=='
+    }
+
+    let(:req_cursor) {
+      ''
     }
 
     context 'when the normal search request' do
       let(:body) {
-        %Q({ "cursor": "#{cursor}", "results": [22776003, 22401006] })
+        %Q({ "cursor": "#{normal_cursor}", "results": [22776003, 22401006] })
       }
 
       it do
@@ -31,13 +40,40 @@ describe Rietveld do
         })
         rietveld = Rietveld.new(conf)
         ret = rietveld.search(nil)
-        ret.should == cursor
+        ret.should == normal_cursor
+      end
+    end
+
+    context 'when the request has 2 pages' do
+      let(:body) {
+        %Q({ "cursor": "#{normal_cursor}", "results": [22776003, 22401006] })
+      }
+
+      it do
+        stub_request(:get, site_url).to_return({
+          body: body,
+          status: 200,
+          headers: {'content-type' => ':application/json; charset=utf-8'}
+        })
+
+        stub_request(:get, get_site_url_with_cursor(normal_cursor)).to_return({
+          body: body,
+          status: 200,
+          headers: {'content-type' => ':application/json; charset=utf-8'}
+        })
+
+        rietveld = Rietveld.new(conf)
+        ret = rietveld.search(nil)
+        ret.should == normal_cursor
+
+        ret = rietveld.search(ret)
+        ret.should == normal_cursor
       end
     end
 
     context 'when the search query reach the end' do
       let(:body) {
-        %Q({ "cursor": "#{cursor}", "results": [] })
+        %Q({ "cursor": "#{normal_cursor}", "results": [] })
       }
 
       it do
